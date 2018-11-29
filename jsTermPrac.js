5f28be858feada45569042f56fa10ee72360c8f9
bmontellano <bmontellano@gmail.com>
2018-11-28 18:40:48 -0800

    Maintenance commit

--- jsTermPrac.js
+++ jsTermPrac.js
@@ -1,5 +1,5 @@
 var nums = [1,20,300,6000]
 var numsDivisibleBy3 = nums.filter(function(num){
-    return num % 3 === 0;
+    if(num % 3 === 0){return num};
 })
 console.log(numsDivisibleBy3);
--- mappingFun.js
+++ mappingFun.js
@@ -68,3 +68,40 @@ class Users extends React.Component {
     <Users list={['Tyler', 'Mikenzi', 'Ryan', 'Michael']} />,
     document.getElementById('app')
   );
+
+  //Finished mapping project
+  class Users extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Friends</h1>
+          <ul>
+            {this.props.list.map( function(friend){
+                if(friend.friend)return <li> {friend.name} </li> 
+            })}
+          </ul>
+          
+          <hr />
+          
+          <h1> Non Friends </h1>
+          <ul>
+              {this.props.list.map( function(f){
+              if(!f.friend)return <li>{f.name}</li>
+            })}
+          </ul>        
+        </div>
+      )
+    }
+  }
+  
+  ReactDOM.render(
+    <Users list={[
+      { name: 'Tyler', friend: true },
+      { name: 'Ryan', friend: true },
+      { name: 'Michael', friend: false },
+      { name: 'Mikenzi', friend: false },
+      { name: 'Jessica', friend: true },
+      { name: 'Dan', friend: false } ]} 
+    />,
+    document.getElementById('app')
+  );
