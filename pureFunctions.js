418f3e8838eefe7021172f7ff3354cfcab43634d
bmontellano <bmontellano@gmail.com>
2018-12-05 16:37:22 -0800

    updated pureFunctions.js

---
+++ pureFunctions.js
@@ -0,0 +1,7 @@
+var ProfilePic extends React.Components {
+    render() {
+        return (
+            <img src={'https://.photobomber.es/profilename/pic/' + this.props.username}></img>
+        )
+    }
+}
