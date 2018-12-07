e7555208e4b51153b3295ff6b01309fa064130f3
bmontellano <bmontellano@gmail.com>
2018-12-06 18:29:35 -0800

    finished implicit, explicit, new Binding and window bindding

--- newWindowBind.js
+++ newWindowBind.js
@@ -1,1 +1,30 @@
 //Testing the new alias 
+
+//New Keyword
+
+var Animal = function(color, name, type) {
+    //this = {}
+    this.color = color;
+    this.name = name;
+    this.type = type;
+}
+
+var kitty = new Animal('Orange', 'Forest','Cat');
+
+console.log("This is the kitty ---> ",kitty);
+
+//Window binding
+//using strict mode will not let you bind this to the window object
+var sayBirthday = function(){
+    'use strict';
+    console.log(this.birthday);
+};
+
+var me = {
+    birthday : '02/18/1987'
+};
+
+//The window object only exists in a browser
+//Running this code in VS code will yield Reference Error
+window.birthday = '02/18/1987'
+sayBirthday();
